cb6c43e4ef4efd45a259e6fdeeacf379ed019498
AliaksandrLeshyk <70918395+AliaksandrLeshyk@users.noreply.github.com>
2021-07-14 01:54:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 
 exports.min = function min (array) {
-    if(!array) return 0;
+    if(!array || array.length == 0) return 0;
     let sorted = array.sort((a, b) => a - b);
     return +sorted[0];
 }
 
 exports.max = function max (array) {
-  if(!array) return 0;
+  if(!array || array.length == 0) return 0;
     let sorted = array.sort((a, b) => a - b);
   return +sorted[sorted.length - 1];
 }
 
 exports.avg = function avg (array) {
-  if(!array) return 0;
+  if(!array || array.length == 0) return 0;
   return array.reduce((acc, item) => acc + item, 0) / array.length;
 }
